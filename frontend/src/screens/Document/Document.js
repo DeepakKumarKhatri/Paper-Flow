bc7bdb4b50c68ba4458d58afd2a43e44ba5c3d3d
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-02-09 11:02:26 +0500

    Better UI for 404 Document + Handled Unexpected errors

--- frontend/src/screens/Document/Document.js
+++ frontend/src/screens/Document/Document.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import pdfFile from "../../assets/CSC_110_PPIT_CDF.pdf";
+import React from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
@@ -7,28 +6,40 @@ import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import style from "../Document/Document.module.css";
 import { decryptLink } from "../../helpers/hashing";
 import { useLocation } from "react-router-dom";
+import noDocumentImage from "../../assets/images/doucment-404.jpg";
 
 const DocumentComp = () => {
   const newPlugin = defaultLayoutPlugin();
   const location = useLocation();
-  const originalLink = decryptLink(location?.state?.urlFile);
+  const originalLink = location?.state?.urlFile
+    ? decryptLink(location.state.urlFile)
+    : null;
 
   return (
     <div className={style["pdfContainer"]}>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        {pdfFile && (
-          <>
-            <Viewer
-              theme={"auto"}
-              fileUrl={originalLink}
-              plugins={[newPlugin]}
-            />
-          </>
+        {originalLink && (
+          <Viewer
+            theme={"auto"}
+            fileUrl={originalLink}
+            plugins={[newPlugin]}
+          />
         )}
-        {!pdfFile && (
-          <>
-            <h1>No PDF</h1>
-          </>
+        {!originalLink && (
+          <div className={style["noDocumentContainer"]}>
+            <img
+              src={noDocumentImage}
+              alt="No Document Found"
+              className={style["noDocumentImage"]}
+            />
+            <h1 className={style["noDocumentMessage"]}>
+              No document found.
+            </h1>
+            <p className={style["noDocumentExplanation"]}>
+              The document you are trying to access is not available. Please
+              check back later or contact support for assistance.
+            </p>
+          </div>
         )}
       </Worker>
     </div>
