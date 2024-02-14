a33decfd98f92c95e973c1f7de5b81c04303f1b5
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-02-14 13:24:32 +0500

    Added dropdown in sidebar

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -6,6 +6,11 @@ import {
   IoPeopleCircleSharp,
   IoPersonSharp,
   IoNotificationsSharp,
+  IoSettings,
+  IoCaretDown,
+  IoCaretUp,
+  IoAccessibilitySharp,
+  IoLogOut,
 } from "react-icons/io5";
 import styles from "./Sidebar.module.css";
 import { Link } from "react-router-dom";
@@ -13,14 +18,14 @@ import sidebar_logo from "../../assets/images/sidebar_logo.png";
 
 const Sidebar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [activeTab, setActiveTab] = useState("");
+  const [userDropdownOpen, setUserDropdownOpen] = useState(false);
+
+  const toggleUserDropdown = () => {
+    setUserDropdownOpen(!userDropdownOpen);
+  };
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
-  };
-
-  const handleSetActiveTab = (tabName) => {
-    setActiveTab(tabName);
   };
 
   useEffect(() => {
@@ -62,76 +67,76 @@ const Sidebar = () => {
       </div>
 
       <ul className={styles.sidebar_menu}>
-        <Link
-          to={"/courses"}
-          className={`${styles.headings} ${
-            activeTab === "Courses" ? styles.active : ""
-          }`}
-          onClick={() => handleSetActiveTab("Courses")}
-        >
+        <li>
+          <div
+            className={`${styles.headings} dropdown_toggle`}
+            onClick={toggleUserDropdown}
+          >
+            <div className={styles.userToggle}>
+              <div>
+                <IoAccessibilitySharp/>
+                <span>User</span>
+              </div>
+              <>{userDropdownOpen ? <IoCaretUp /> : <IoCaretDown />}</>
+            </div>
+          </div>
+          <ul
+            className={`${styles.dropdown_menu} ${
+              userDropdownOpen ? styles.active : ""
+            }`}
+          >
+            <Link className={styles.dropdownHeadings} to="">
+              <li>
+                <IoPersonSharp className={styles.icon} />
+                <span>View Profile</span>
+              </li>
+            </Link>
+            <Link to="" className={styles.dropdownHeadings}>
+              <li>
+                <IoLogOut className={styles.icon} />
+                <span>Logout</span>
+              </li>
+            </Link>
+          </ul>
+        </li>
+        <Link to={"/courses"} className={styles.headings}>
           <li>
             <AiIcons.AiFillHome />
             <span>Courses</span>
           </li>
         </Link>
-        <Link
-          className={`${styles.headings} ${
-            activeTab === "My Solutions" ? styles.active : ""
-          }`}
-          onClick={() => handleSetActiveTab("My Solutions")}
-        >
+        <Link className={styles.headings}>
           <li>
             <IoIcons.IoIosPaper />
             <span>Contributions</span>
           </li>
         </Link>
 
-        <Link
-          to={"/bookmarks"}
-          className={`${styles.headings} ${
-            activeTab === "My Bookmarks" ? styles.active : ""
-          }`}
-          onClick={() => handleSetActiveTab("My Bookmarks")}
-        >
+        <Link to={"/bookmarks"} className={styles.headings}>
           <li>
             <IoIcons.IoIosSave />
             <span>Bookmarks</span>
           </li>
         </Link>
 
-        <Link
-          className={`${styles.headings} ${
-            activeTab === "Notifications" ? styles.active : ""
-          }`}
-          onClick={() => handleSetActiveTab("Notifications")}
-        >
+        <Link className={styles.headings}>
           <li>
             <IoNotificationsSharp />
             <span>Notifications</span>
           </li>
         </Link>
 
-        <Link
-          className={`${styles.headings} ${
-            activeTab === "Leaderboard" ? styles.active : ""
-          }`}
-          onClick={() => handleSetActiveTab("Leaderboard")}
-        >
+        <Link className={styles.headings}>
           <li>
             <IoPeopleCircleSharp />
             <span>Leaderboard</span>
           </li>
         </Link>
 
-        <Link
-          className={`${styles.headings} ${
-            activeTab === "About Creator" ? styles.active : ""
-          }`}
-          onClick={() => handleSetActiveTab("About Creator")}
-        >
+        <Link className={styles.headings}>
           <li>
-            <IoPersonSharp />
-            <span>About Creator</span>
+            <IoSettings />
+            <span>Support</span>
           </li>
         </Link>
       </ul>
